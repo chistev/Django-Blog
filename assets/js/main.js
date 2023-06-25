f379beca7fdc8a5827a1e9bdd17cc4846ce32c90
chistev <stephenowabie@gmail.com>
2023-06-25 10:16:57 +0100

    updated the like and unlike functionality to make both like buttons on the detail page to update simultaneously when either one is clicked without having to reload the page

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,43 +5,48 @@ $('.like-count').each(function() {
     var count = parseInt($(this).text());
     var count_str = count === 1 ? '1 like' : count + ' likes';
     $(this).text(count_str);
-  });
+});
 
-  // Update the count string when the like button is clicked
-$('.like-button').click(function(e){
-    e.preventDefault()
-    var this_ = $(this)
-    var likeURL = this_.attr('data-href')
+// Update the count string when the like buttons are clicked
+$('.like-button').click(function(e) {
+    e.preventDefault();
+    var this_ = $(this);
+    var likeURL = this_.attr('data-href');
 
-     if (likeURL){
+    if (likeURL) {
         $.ajax({
             url: likeURL,
             method: 'GET',
             data: {},
-            success: function(data){
-                console.log(data)
-                var count = data.likes_count
-                var count_str = data.count_str
-                var like_icon = this_.find('.fas')
-                var like_count = this_.find('.like-count')
-                
-                if (data.liked){
-                    like_icon.css('color', 'salmon')
-                    like_count.text(count + ' ' + count_str)
-                    like_count.css('color', 'salmon')
-                }
-                else{
-                    like_icon.css('color', 'white')
-                    like_count.text(count + ' ' + count_str)
-                    like_count.css('color', '')
-                }
-            }, error: function(error){
+            success: function(data) {
+                console.log(data);
+                var count = data.likes_count;
+                var count_str = data.count_str;
+
+                // Update the like count and style for both buttons
+                $('.like-button').each(function() {
+                    var like_icon = $(this).find('.article-like-style');
+                    var like_count = $(this).find('.like-count');
+
+                    if (data.liked) {
+                        like_icon.css('color', 'salmon');
+                        like_count.text(count + ' ' + count_str);
+                        like_count.css('color', 'salmon');
+                    } else {
+                        like_icon.css('color', 'white');
+                        like_count.text(count + ' ' + count_str);
+                        like_count.css('color', '');
+                    }
+                });
+            },
+            error: function(error) {
                 console.error(error);
-                console.error('error')
+                console.error('error');
             }
-        })
+        });
     }
-})
+});
+
 
 // delete post functionality
 
